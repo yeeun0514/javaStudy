165c9cc9ad818cc3fd9dcaec90851ac652b11e6c
yeeun0514 <yeeun.g.sin@gmail.com>
2022-04-14 08:59:45 +0900

    [#1] type : stream concept practice body : String sort (String[], List<String>)

---
+++ javaStudy/src/streamPractice/stream02.java
@@ -0,0 +1,24 @@
+package streamPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+ * String sort using stream
+ * String[], List<String>
+ */
+public class stream02 {
+
+	public static void main(String[] args) {
+		String[] nameArr = {"IronMan", "Captain", "Hulk", "Thor", "Black Panther", "Spidey", "Black Widow"};
+		List<String> nameList = Arrays.asList(nameArr);
+		
+		Stream<String> arrayStream = Arrays.stream(nameArr);
+		Stream<String> nameStream = nameList.stream();
+		
+		nameStream.sorted().forEach(System.out::println);
+		arrayStream.sorted().forEach(System.out::println);
+	}
+
+}
