ffea6dd244feeb1bcd61de049fa94c21c0680902
yeeun0514 <yeeun.g.sin@gmail.com>
2022-05-04 13:24:58 +0900

    feat : stream conversion practice
    원시 stream <-> stream
    
    issues : #1

---
+++ javaStudy/src/streamPractice/Stream06.java
@@ -0,0 +1,24 @@
+/*
+ * 원시 Stream <-> Stream
+ */
+package streamPractice;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Stream06 {
+
+	public static void main(String[] args) {
+		// IntStream -> Stream<Integer>
+		IntStream.range(1, 5).mapToObj(i -> "a" + i).forEach(System.out::print);
+		
+		System.out.println();
+		
+		// Stream<Double> -> IntStream -> Stream<String>
+		Stream.of(1.0, 2.0, 3.0)
+			.mapToInt(Double::intValue)
+			.mapToObj(i -> "b" + i)
+			.forEach(System.out::println);
+	}
+
+}
