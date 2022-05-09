feeda8255afc6e12861570534a95b67415033ab3
yeeun0514 <yeeun.g.sin@gmail.com>
2022-05-10 08:32:26 +0900

    feat : static practice
    정보처리기사 5/7 시험
    
    issues : #2

---
+++ javaStudy/src/staticPractice/static01.java
@@ -0,0 +1,32 @@
+/**
+ * 실기 static 문제
+ */
+package staticPractice;
+
+class A {
+	int a;
+	int b;
+}
+
+public class static01 {
+
+	static void func1(A m){
+		m.a *= 10;
+	}
+	
+	static void func2(A m){
+		m.a += m.b;
+	}
+	
+	public static void main(String[] args) {
+		A m = new A();
+		
+		m.a = 100;
+		func1(m);
+		m.b = m.a;
+		func2(m);
+		
+		System.out.printf("%d", m.a);
+	}
+
+}
