a99d665648b848c1f4437eb48e4f5612a8e0e87a
yeeun0514 <yeeun.g.sin@gmail.com>
2022-05-23 08:40:39 +0900

    feat : Stream FlatMap List, Array
    issues : #1

---
+++ javaStudy/src/streamPractice/Stream12.java
@@ -0,0 +1,52 @@
+/**
+ * Stream FlatMap
+ * Object List
+ */
+package streamPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Stream12 {
+
+	public static void main(String[] args) {
+		List <Student> students = Arrays.asList(
+				new Student(80, 90, 75),
+				new Student(70, 100, 75),
+				new Student(85, 90, 85),
+				new Student(80, 100, 90));
+		
+		students.stream().flatMapToInt(student ->
+				IntStream.of(student.getKor(), student.getEng(), student.getMath()))
+				.average()
+				.ifPresent(avg -> System.out.println(Math.round(avg * 10) / 10.0));
+		
+	}
+
+}
+
+class Student {
+	private int kor;
+	private int eng;
+	private int math;
+	
+	public Student(int kor, int eng, int math) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+	
+}
